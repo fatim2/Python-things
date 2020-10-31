71f960ddb6ab64eb30d3f5d17b40fa5b359a8fe3
Fatim <fatimazahrablk222@gmail.com>
2020-10-31 14:32:43 +0100

    last commit

--- pdfreader.py
+++ pdfreader.py
@@ -7,8 +7,9 @@ pages = pdfReader.numPages
 #print the number of pages in the pdf
 print(pages)
 speaker = pyttsx3.init()
-#if you wanna choose from where to start the reading
-page = pdfReader.getPage(14) #reading page i+1
-text = page.extractText()
-speaker.say(text)
-speaker.runAndWait()
+choose from where to start the reading
+for num in range(14, pages):
+    page = pdfReader.getPage(14) #reading page i+1
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
