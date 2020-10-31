d7387b75d1b2db2c1b99bb3c70ee572c7d9e258a
Fatim <fatimazahrablk222@gmail.com>
2020-10-31 13:34:51 +0100

    second commit

--- pdfreader.py
+++ pdfreader.py
@@ -1,6 +1,14 @@
-#install the library pyttsx3
 import pyttsx3
+import PyPDF2
+#the pdf name
+book = open('py.pdf','rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+#print the number of pages in the pdf
+print(pages)
 speaker = pyttsx3.init()
-speaker.say('Hello World') #test your speakers
+#if you wanna choose from where to start the reading
+page = pdfReader.getPage(14) #reading page i+1
+text = page.extractText()
+speaker.say(text)
 speaker.runAndWait()
-#########
